6671eee119eac1898938aa015212bde51dc0151c
AaronADeveloper <aaronadeveloper@gmail.com>
2020-11-12 18:35:25 -0500

    completed API request and populated DOM elements with API returned data

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader'); 
+
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+
+function hideLoadingSpinner() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true
+    }
+}
+
 // Get quotes from API
 let apiQuotes = [];
 
 const showNewQuote = () => {
+    showLoadingSpinner();
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
-    console.log(quote)
+
+    //check if Author field is blank and render 'unknown' if it is
+    if(!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+
+    //check Quote length to determine font styling
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote')
+    } else {
+        quoteText.classList.remove('long-quote')
+    }
+    
+    //set Quote, hide loading spinner
+    quoteText.textContent = quote.text;
+    hideLoadingSpinner();
 }
 
 async function getQuotes() {
@@ -17,6 +55,17 @@ async function getQuotes() {
         console.log(error)
     }
 }
+
+//Tweet quote
+function tweetQuote() {
+    const twitterURL = `https:twitter.com/intent/tweet?text=${quoteText.textContent} -${authorText.textContent}`;
+    window.open(twitterURL, '_blank');
+}
+
+//Event listeners
+newQuoteBtn.addEventListener('click', showNewQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
 
 // On load
 getQuotes();
